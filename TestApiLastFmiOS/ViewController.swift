e4e3d24d44e8f43b840d7a764406bc416a5c2d1a
Gerardo Mascayano <gerardo.mascayano@gmail.com>
2018-02-18 19:39:31 -0300

    - Commit inicial

---
+++ TestApiLastFmiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestApiLastFmiOS
+//
+//  Created by Gerardo Mascayano on 18-02-18.
+//  Copyright © 2018 Gerardo Mascayano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
